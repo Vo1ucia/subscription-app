186102d023f73a89e1766c1c4396bf74d9063e8a
Vo1ucia <sandralys58@gmail.com>
2025-08-05 15:10:33 +0200

    cors solution

--- backend/Subscription-Manager/src/main/java/com/abontrack/Subscription/Manager/config/SecurityConfig.java
+++ backend/Subscription-Manager/src/main/java/com/abontrack/Subscription/Manager/config/SecurityConfig.java
@@ -80,12 +80,14 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
-        configuration.setAllowedOrigins(Arrays.asList("hhttps://subscription-app-wine.vercel.app"));
+        configuration.setAllowedOrigins(Arrays.asList(
+            "http://localhost:4200",
+            "https://subscription-app-wine.vercel.app"
+        ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT","PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With"));
         configuration.setAllowCredentials(true);
-        
+
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
